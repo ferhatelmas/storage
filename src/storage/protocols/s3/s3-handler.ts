35a74bc845cfb37f3daa3a041cc9425f3e337922
ferhat elmas <elmas.ferhat@gmail.com>
2025-11-06 08:26:16 +0100

    fix: typo in user facing error message
    Signed-off-by: ferhat elmas <elmas.ferhat@gmail.com>

--- src/storage/protocols/s3/s3-handler.ts
+++ src/storage/protocols/s3/s3-handler.ts
@@ -979,7 +979,7 @@ export class S3ProtocolHandler {
       (o) => ({
         Key: o.Key,
         Code: 'AccessDenied',
-        Message: "You do not have permission to delete this object or the object doesn't exists",
+        Message: "You do not have permission to delete this object or the object doesn't exist",
       })
     )
 
